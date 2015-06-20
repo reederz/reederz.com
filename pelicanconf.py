27998f2db65b6a2e0925563a34af886f544e56ac
Justas Azna <ja@fadeit.dk>
2015-06-20 21:29:37 +0200

    Add links to keybase and onename

--- pelicanconf.py
+++ pelicanconf.py
@@ -21,15 +21,12 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-#LINKS = (('Pelican', 'http://getpelican.com/'),
-         #('Python.org', 'http://python.org/'),
-         #('Jinja2', 'http://jinja.pocoo.org/'),
-         #('You can modify those links in your config file', '#'),)
 
 # Social widget
 SOCIAL = (('github', 'https://github.com/reederz'),
-          ('twitter', 'https://twitter.com/reederz'),)
+          ('twitter', 'https://twitter.com/reederz'),
+          ('+justas', 'https://onename.com/justas'),
+          ('keybase.io/reederz', 'https://keybase.io/reederz'),)
 
 # Comments
 DISQUS_SITENAME = "reederz"
